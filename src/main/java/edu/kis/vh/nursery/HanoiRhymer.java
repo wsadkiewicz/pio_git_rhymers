58ffcb29c7fc586ecbf0935cf623e68da7d62c0f
Kawalec <251166@edu.p.lodz.pl>
2025-03-13 15:32:32 +0100

    2, poprawiono wcięcia w HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,13 @@
 package edu.kis.vh.nursery;
-
-public class HanoiRhymer extends defaultCountingOutRhymer {
-
-int totalRejected = 0;
-
-    public int reportRejected() {
+public class HanoiRhymer extends defaultCountingOutRhymer{
+    int totalRejected = 0;
+    public int reportRejected(){
         return totalRejected;
     }
-
-    public void countIn(int in) {
+    public void countIn(int in){
     if (!callCheck() && in > peekaboo())
-            totalRejected++;
-            else
-                super.countIn(in);
+        totalRejected++;
+    else
+        super.countIn(in);
     }
 }
