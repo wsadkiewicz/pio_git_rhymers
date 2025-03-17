ed9e67413a5acb1dbbb12a992312a0587ceb403f
wsadkiewicz <248471@edu.p.lodz.pl>
2025-03-17 20:07:57 +0100

    5. Zmiana nazw pól w klasach

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,17 +2,17 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public defaultCountingOutRhymer tempRhymer = new defaultCountingOutRhymer();
 
     @Override
     public int countOut() {
         while (!callCheck())
-            temp.countIn(super.countOut());
+            tempRhymer.countIn(super.countOut());
 
-        int ret = temp.countOut();
+        int ret = tempRhymer.countOut();
 
-        while (!temp.callCheck())
-            countIn(temp.countOut());
+        while (!tempRhymer.callCheck())
+            countIn(tempRhymer.countOut());
 
         return ret;
     }
