5a9b58aef8ae99c3267354a37f753d1ecb942f20
IgorKawalec <251166@edu.p.lodz.pl>
2025-03-19 18:06:42 +0100

    Rebase class-method-naming i merge zmian

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.Test1defaultCountingOutRhymer;
+import edu.kis.vh.nursery.HanoiRhymerTest1;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
@@ -10,8 +10,8 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
         
-        defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+        Test1defaultCountingOutRhymer[] rhymers = { factory.Test1GetStandardRhymer(), factory.Test1GetFalseRhymer(),
+                factory.Test1GetFIFORhymer(), factory.Test1GetHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
@@ -28,7 +28,7 @@ class RhymersDemo {
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymerTest1) rhymers[3]).reportRejected());
         
     }
     
