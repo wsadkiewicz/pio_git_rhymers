5a213c18d08d19dae2f3f161323311522578fac8
IgorKawalec <251166@edu.p.lodz.pl>
2025-03-19 17:38:28 +0100

    Poprawa hanoi attempt 1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,14 +1,19 @@
 package edu.kis.vh.nursery;
-public class HanoiRhymer extends defaultCountingOutRhymer{
+
+public class HanoiRhymer extends defaultCountingOutRhymer {
+
     int totalRejected = 0;
-    public int reportRejected(){
+
+    public int reportRejected() {
         return totalRejected;
     }
-    public void countIn(int in){
-    if (!callCheck() && in > peekaboo())
-        totalRejected++;
-    else
-        super.countIn(in);
+
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+
     }
-    
+
 }
