db59271523fea1be290adda903bd3817a6f22372
wsadkiewicz <248471@edu.p.lodz.pl>
2025-04-09 18:36:21 +0200

    13. Utworzenie metody statycznej testRhymers w pliku RhymersDemo.java

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,28 +11,32 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-        
+
         for (int i = 1; i < COUNTER; i++)
             for (int j = 0; j < RHYMERS; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < COUNTER; i++)
             rhymers[RHYMERS].countIn(rn.nextInt(BOUND));
 
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].isEmpty())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[RHYMERS]).reportRejected());
-        
     }
 
     public static int getCOUNTER() {
